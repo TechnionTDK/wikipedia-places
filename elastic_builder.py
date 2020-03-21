9eb9476b7fd496d63899bec69676593cfa5dc98d
Nerya <neryahadad@gmail.com>
2020-03-21 03:49:49 +0200

    refactoring

--- elastic_builder.py
+++ elastic_builder.py
@@ -1,39 +1,36 @@
 import requests
 import json
 
-allLabels = []
-labelsToUrls = {}
-#labelsToCoordinates = {}
-urlsToAbstract = {}
-elasticDocs = {}
-
-# loading data
-with open(r"input/wiki_hebrew_labels.json", "r", encoding='utf-8') as read_file:
-    labelsAndUrls = json.load(read_file)
-with open(r"input/wiki_hebrew_abstracts.json", "r", encoding='utf-8') as read_file:
-    abstractsAndUrls = json.load(read_file)
-
-for labelDict in labelsAndUrls:
-    label = labelDict['rdfs:label']
-    allLabels.append(label)
-    labelsToUrls[label] = labelDict['uri']
-
-for abstractDict in abstractsAndUrls:
-    url = abstractDict['uri']
-    urlsToAbstract[url] = abstractDict['dbo:abstract']
-
-
-# get coordinates
 S = requests.Session()
 
-URL = "https://he.wikipedia.org/w/api.php"
 
-PARAMS = {
-    "action": "query",
-    "format": "json",
-    "titles": "",
-    "prop": "coordinates"
-}
+class dataLoad:
+    def __init__(self):
+        # data structures
+        self.allLabels = []
+        self.labelsToUrls = {}
+        self.urlsToAbstract = {}
+
+        # get data
+        self.__labelsAndUrlsFromJson()
+        self.__abstractsFromJson()
+
+    def __labelsAndUrlsFromJson(self):
+        with open(r"input/wiki_hebrew_labels.json", "r", encoding='utf-8') as read_file:
+            labelsAndUrls = json.load(read_file)
+        # saves in better data structures
+        for labelDict in labelsAndUrls:
+            label = labelDict['rdfs:label']
+            self.allLabels.append(label)
+            self.labelsToUrls[label] = labelDict['uri']
+
+    def __abstractsFromJson(self):
+        with open(r"input/wiki_hebrew_abstracts.json", "r", encoding='utf-8') as read_file:
+            abstractsAndUrls = json.load(read_file)
+        # saves in better data structures
+        for abstractDict in abstractsAndUrls:
+            url = abstractDict['uri']
+            self.urlsToAbstract[url] = abstractDict['dbo:abstract']
 
 
 # allLabels = ["הקרב על גבעת התחמושת", "בית הספר לשוטרים (ירושלים)", "בית הספר רנה קסין", "קריית הממשלה (מזרח ירושלים)", "שכונות הבריח", "קריית הלאום", "יד הזיכרון למגיני ירושלים",
@@ -42,44 +39,52 @@ PARAMS = {
 
 
 
-for label in allLabels:
-    PARAMS["titles"] = label
-    for k, v in S.get(url=URL, params=PARAMS).json()['query']['pages'].items():
-        if 'coordinates' in v:
-            fullCoordinates = v['coordinates'][0]
-            coordinates = {
-                "lat": fullCoordinates['lat'],
-                "lon": fullCoordinates['lon']
-            }
-            url = labelsToUrls[label]
-            doc = {
-                "label": label,
-                "coordinates": coordinates,
-                "url": url,
-                "abstract": urlsToAbstract[url]
-            }
-            elasticDocs.append(doc)
-            # print("Latitute: " + str(v['coordinates'][0]['lat']))
-            # print("Longitude: " + str(v['coordinates'][0]['lon']))
-        # else:
-        #     del labelsToUrls[label]
-# print(labelsToCoordinates)
-# print("#coordinatesSize: ", labelsToCoordinates.__len__())
-# print("#urlsSize: ", labelsToUrls.__len__())
-#
-# testLabel = allLabels[allLabels.__len__()-1]
-# print(testLabel, labelsToCoordinates[testLabel])
-for doc in elasticDocs:
-    with open('output/' + doc["label"] + '.json', 'w', encoding='utf-8') as outfile:
-        json.dump(doc, outfile, ensure_ascii=False)
-    res = S.post(url='http://localhost:9200/nerya/_doc/'+doc["label"], headers={'Content-Type': 'application/json'}, json=doc)
+
+def saveWithCoordinates(data):
+    #init
+    URL = "https://he.wikipedia.org/w/api.php"
+    PARAMS = {
+        "action": "query",
+        "format": "json",
+        "titles": "",
+        "prop": "coordinates"
+    }
+    docs = []
+
+    #saves
+    for label in data.allLabels:
+        PARAMS["titles"] = label
+        # R = S.get(url=URL, params=PARAMS)
+        # DATA = R.json()
+        # PAGES = DATA['query']['pages']
+        # for k, v in PAGES.items():
+        for k, v in S.get(url=URL, params=PARAMS).json()['query']['pages'].items():
+            if 'coordinates' in v:
+                fullCoordinates = v['coordinates'][0]
+                coordinates = {
+                    "lat": fullCoordinates['lat'],
+                    "lon": fullCoordinates['lon']
+                }
+                url = data.labelsToUrls[label]
+                doc = {
+                    "label": label,
+                    "coordinates": coordinates,
+                    "url": url,
+                    "abstract": data.urlsToAbstract[url]
+                }
+                docs.append(doc)
+    return docs
+
+
+def elasticBuilder(elasticDocs):
+    for doc in elasticDocs:
+        print("start indexing label " + doc["label"] + "...")
+        # with open('output/' + doc["label"] + '.json', 'w', encoding='utf-8') as outfile:
+        #     json.dump(doc, outfile, ensure_ascii=False)
+        res = S.post(url='http://localhost:9200/nerya/_doc/'+doc["label"], headers={'Content-Type': 'application/json'}, json=doc)
+        print("start indexing label " + doc["label"] + "...")
 
 # for label in allLabels:p
-#     elas_doc = {
-#         "label": label,
-#         "coordinates": labelsToCoordinates[label]
-#     }
-#
 #     # es.index(index="nerya", doc_type='_doc', id=label,body=doc)
 #     print("start indexing lable "+label+ "...")
 #     res = S.post(url='http://localhost:9200/nerya/_doc/'+label, headers={'Content-Type': 'application/json'}, json=elas_doc)
@@ -92,15 +97,10 @@ for doc in elasticDocs:
 #         print(label)
 
 
+def main():
+    data = dataLoad()
+    elasticDocs = saveWithCoordinates(data)
+    elasticBuilder(elasticDocs)
 
-# code example:
-# R = S.get(url=URL, params=PARAMS)
-# DATA = R.json()
-# PAGES = DATA['query']['pages']
-# print(PAGES.items())
-# print(PAGES.items)
-# for k, v in PAGES.items():
-
-
-# ירושלים info:
-# dict_items([('325', {'pageid': 325, 'ns': 0, 'title': 'ירושלים', 'coordinates': [{'lat': 31.78264861, 'lon': 35.2193299, 'primary': '', 'globe': 'earth'}]})])
+if __name__ == "__main__":
+    main()
