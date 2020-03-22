5d22e5af469c2fc14ba96870a5f54edad6dfd489
Nerya <neryahadad@gmail.com>
2020-03-22 21:40:33 +0200

    refactoring

--- elastic_builder.py
+++ elastic_builder.py
@@ -1,9 +1,6 @@
-import urllib.parse
-
 import requests
 import json
-
-from idna import unicode
+import urllib.parse
 
 S = requests.Session()
 
@@ -15,20 +12,9 @@ class dataLoad:
         self.labelsToUrls = {}
         self.urlsToAbstract = {}
 
-
         # get data
         self.__labelsAndUrlsFromJson()
         self.__abstractsFromJson()
-
-        # print("size:")
-        # print("self.allLabels: ", self.allLabels.__len__())
-        # print("self.labelsToUrls: ", self.labelsToUrls.__len__())
-        # print("self.urlsToAbstract: ", self.urlsToAbstract.__len__())
-
-        testLabel = "מתמטיקה"
-        url = self.labelsToUrls[testLabel]
-        print(testLabel, url)
-        print(self.urlsToAbstract[url])
 
 
     def __labelsAndUrlsFromJson(self):
@@ -41,43 +27,13 @@ class dataLoad:
             self.labelsToUrls[label] = labelDict['uri']
 
 
-
-
-
-            # self.labelsToUrls[label] = urllib.parse.quote(labelDict['uri'])
-            # self.labelsToUrls[label] = labelDict['uri'].strip().encode('utf-8')
-            # url = unicode(labelDict['uri'])#.strip().encode('idna')
-            # # url2 = labelDict['uri'].strip().encode('idna')
-            # url3 = labelDict['uri'].strip().encode('utf-8')
-            # url4 = labelDict['uri'].encode('idna')
-
-
-
-
-
-
     def __abstractsFromJson(self):
         with open(r"input/wiki_hebrew_abstracts.json", "r", encoding='utf-8') as read_file:
             abstractsAndUrls = json.load(read_file)
         # saves in better data structures
         for abstractDict in abstractsAndUrls:
-
             url = urllib.parse.unquote(abstractDict['uri'])
             self.urlsToAbstract[url] = abstractDict['dbo:abstract']
-
-
-
-
-            # url2 = abstractDict['uri'].strip().encode('idna')
-            #             # url3 = abstractDict['uri'].strip().encode('utf-8')
-            #             # url4 = abstractDict['uri'].encode('idna')
-            #             # print(url)
-
-
-# allLabels = ["הקרב על גבעת התחמושת", "בית הספר לשוטרים (ירושלים)", "בית הספר רנה קסין", "קריית הממשלה (מזרח ירושלים)", "שכונות הבריח", "קריית הלאום", "יד הזיכרון למגיני ירושלים",
-#           "הרי יהודה", "סנהדריה המורחבת", "ישיבת תפארת צבי", "ישיבת מיר", "נחל צופים", "נבי סמואל", "שדרות בר-לב",
-#           "גן העצמאות (ירושלים)", "דרך שכם", "קבר רחל", "תחנת הרכבת ההיסטורית של ירושלים", "פארק אירופה", "דיסנילנד פריז"]
-
 
 
 
@@ -95,10 +51,6 @@ def saveWithCoordinates(data):
     #saves
     for label in data.allLabels:
         PARAMS["titles"] = label
-        # R = S.get(url=URL, params=PARAMS)
-        # DATA = R.json()
-        # PAGES = DATA['query']['pages']
-        # for k, v in PAGES.items():
         for k, v in S.get(url=URL, params=PARAMS).json()['query']['pages'].items():
             if 'coordinates' in v:
                 fullCoordinates = v['coordinates'][0]
@@ -126,22 +78,8 @@ def saveWithCoordinates(data):
 def elasticBuilder(elasticDocs):
     for doc in elasticDocs:
         print("start indexing label " + doc["label"] + "...")
-        # with open('output/' + doc["label"] + '.json', 'w', encoding='utf-8') as outfile:
-        #     json.dump(doc, outfile, ensure_ascii=False)
         res = S.post(url='http://localhost:9200/nerya/_doc/'+doc["label"], headers={'Content-Type': 'application/json'}, json=doc)
         print("start indexing label " + doc["label"] + "...")
-
-# for label in allLabels:p
-#     # es.index(index="nerya", doc_type='_doc', id=label,body=doc)
-#     print("start indexing lable "+label+ "...")
-#     res = S.post(url='http://localhost:9200/nerya/_doc/'+label, headers={'Content-Type': 'application/json'}, json=elas_doc)
-#     print("finished indexing lable "+label+"!")
-
-
-
-# for label in labels:
-#     if label not in results:
-#         print(label)
 
 
 def main():
