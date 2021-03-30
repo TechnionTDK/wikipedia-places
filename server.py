c6b31483bae8743c635fb504069378c426cb6dd9
Nerya <neryahadad@gmail.com>
2021-03-31 01:14:48 +0300

    mainly read_me

--- server.py
+++ server.py
@@ -1,9 +1,9 @@
 
 from flask import Flask, request
 import json
-# instantiate the app
 from search import search
 
+# instantiate the app
 app = Flask(__name__)
 
 @app.route("/wiki_by_place")
@@ -18,9 +18,12 @@ if __name__ == "__main__":
     app.debug = True
     app.run(host='0.0.0.0', port='80')
 
-""" HTML  request would be
-http://132.69.8.7/wiki_by_place?radius=xxx&lat=xxx&lon=xxx
-example:
-http://132.69.8.7/wiki_by_place?radius=1km&lat=32.7775&lon=35.02166667
+""" HTML request would be
+http://132.69.8.7/wiki_by_place?radius=xxx&lat,lon=xxx,xxx  
+http://132.69.8.7/wiki_by_place?lat,lon=xxx,xxx&radius=xxx  
+Examples - up to 1km from the Technion:  
+http://132.69.8.7/wiki_by_place?radius=1km&lat,lon=32.7775,35.02166667  
+http://132.69.8.7/wiki_by_place?radius=1000m&lat,lon=32.7775,35.02166667  
+http://132.69.8.7/wiki_by_place?lat,lon=32.7775,35.02166667&radius=100000cm  
 """
 
