f22e8d1a546f9353f0dba9df21d5f46e3cb9f939
Raz Levi <razle102030@gmail.com>
2022-06-01 12:55:35 +0300

    support pagination

--- search.py
+++ search.py
@@ -3,26 +3,8 @@ import constants
 import utils
 
 
-def search(params: list) -> list:
-    # query = {
-    #     "query": {
-    #         "bool": {
-    #             "filter": {
-    #                 "geo_distance": {
-    #                     "distance": params[0],
-    #                     "pin.location": {
-    #                         "lat": params[1],
-    #                         "lon": params[2]
-    #                     }
-    #                 }
-    #             }
-    #         }
-    #     }
-    # }
-
+def search_all(params: list) -> list:
     query = {
-        "from": 0,
-        "size": 10,
         "query": {
             "bool": {
                 "filter": {
@@ -37,7 +19,31 @@ def search(params: list) -> list:
             }
         }
     }
+    return search(query=query, params=params)
 
+
+def search_pagination(params: list, from_index: int, size: int) -> list:
+    query = {
+        "from": from_index,
+        "size": size,
+        "query": {
+            "bool": {
+                "filter": {
+                    "geo_distance": {
+                        "distance": params[0],
+                        "pin.location": {
+                            "lat": params[1],
+                            "lon": params[2]
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return search(query=query, params=params)
+
+
+def search(query: dict, params: list) -> list:
     elastic_client = utils.elastic_connect()
     res = elastic_client.search(index=constants.ELASTIC_INDEX, body=query)
     source_coords = (float(params[1]), float(params[2]))
